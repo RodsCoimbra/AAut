7a9868585da3ab82dd7ffc5f7f255ba56fc0bf96
Rodrigo Pereira <rodrigonobrepereira2002@gmail.com>
2023-10-07 20:19:59 +0100

    aaaaa

--- Projeto1_Parte2/Projeto2_modelos.py
+++ Projeto1_Parte2/Projeto2_modelos.py
@@ -18,11 +18,9 @@ SSE_soma = 0
 # Linear
 reg1 = linear_model.LinearRegression()
 reg2 = linear_model.LinearRegression()
-SSEFinal1 = 500
-SSEFinal2 = 500
 finalLinearM1 = linear_model.LinearRegression()
 finalLinearM2 = linear_model.LinearRegression()
-for [(idx_train, idx_teste), (idx_train2, idx_teste2)] in zip(kf.split(x1), kf.split(x2)):
+""" for [(idx_train, idx_teste), (idx_train2, idx_teste2)] in zip(kf.split(x1), kf.split(x2)):
     SSE_final_array = []
     reg1.fit(x1[idx_train], y1[idx_train])
     reg2.fit(x2[idx_train2], y2[idx_train2])
@@ -30,9 +28,62 @@ for [(idx_train, idx_teste), (idx_train2, idx_teste2)] in zip(kf.split(x1), kf.s
     y_prever2 = reg2.predict(x)
     SSE_final_array = np.where((y - y_prever1)**2 > (y-y_prever2)**2,
                                (y-y_prever2)**2, (y - y_prever1)**2)
-    SSE_soma += SSE_final_array.sum()
+    SSE_soma += SSE_final_array.sum() """
 
-print("\nMédia Linear SSE: ", SSE_soma/kf.get_n_splits())
+SSE_final_array = []
+reg1.fit(x1, y1)
+reg2.fit(x2, y2)
+y_prever1 = reg1.predict(x)
+y_prever2 = reg2.predict(x)
+indices = np.where((y - y_prever1)**2 > (y-y_prever2)**2, 0, 1)
+x_cluster1 = []
+y_cluster1 = []
+x_cluster2 = []
+y_cluster2 = []
+
+for idx,j in enumerate(indices):
+    if j == 0:
+        x_cluster2 = np.append(x_cluster2, x[idx])
+        y_cluster2 = np.append(y_cluster2, y[idx])
+    else:
+        x_cluster1 = np.append(x_cluster1, x[idx])
+        y_cluster1 = np.append(y_cluster1, y[idx])
+x_cluster1 = x_cluster1.reshape(round(x_cluster1.shape[0]/4), 4)
+x_cluster2 = x_cluster2.reshape(round(x_cluster2.shape[0]/4), 4)
+
+
+reg1 = linear_model.LinearRegression()
+reg2 = linear_model.LinearRegression()
+reg1.fit(x_cluster1, y_cluster1)
+reg2.fit(x_cluster2, y_cluster2)
+y_prever1 = reg1.predict(x)
+y_prever2 = reg2.predict(x)
+print(indices)
+x_cluster1 = []
+y_cluster1 = []
+x_cluster2 = []
+y_cluster2 = []
+
+for idx,oi in enumerate(indices):
+    if oi == 0:
+        x_cluster2 = np.append(x_cluster2, x[idx])
+        y_cluster2 = np.append(y_cluster2, y[idx])
+    else:
+        x_cluster1 = np.append(x_cluster1, x[idx])
+        y_cluster1 = np.append(y_cluster1, y[idx])
+x_cluster1 = x_cluster1.reshape(round(x_cluster1.shape[0]/4), 4)
+x_cluster2 = x_cluster2.reshape(round(x_cluster2.shape[0]/4), 4)
+
+
+reg1.fit(x_cluster1, y_cluster1)
+reg2.fit(x_cluster2, y_cluster2)
+y_prever1 = reg1.predict(x)
+y_prever2 = reg2.predict(x)
+SSE_final_array = np.where((y - y_prever1)**2 > (y-y_prever2)**2,
+                               (y-y_prever2)**2, (y - y_prever1)**2)
+SSE_soma += SSE_final_array.sum()
+print("SSE: ", SSE_soma)
+""" print("\nMédia Linear SSE: ", SSE_soma/kf.get_n_splits())
 SSE_final_array = []
 reg1.fit(x, y)
 y_prever3 = reg1.predict(x)
@@ -47,4 +98,4 @@ finalLinearM2.fit(x2, y2)
 yFinal2 = finalLinearM2.predict(xFinal)
         
 yFinal = np.hstack((yFinal1,yFinal2))
-np.save("Projeto1_Parte2/Dados/Y_final.npy",yFinal)
+np.save("Projeto1_Parte2/Dados/Y_final.npy",yFinal) """
